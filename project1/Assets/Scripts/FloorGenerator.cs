fbc4a22415d1a1ab43f95605047d1606333022bd
Alex Zheng <20850333+prototype-A@users.noreply.github.com>
2020-02-22 20:46:40 -0500

    Keep sprite orientation after movement, Implement basic procedural tilemap generator

--- project1/Assets/Scripts/FloorGenerator.cs
+++ project1/Assets/Scripts/FloorGenerator.cs
@@ -1,15 +1,120 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class FloorGenerator : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
 
+    // Chance for tile to be ground
+    [Range(0, 100)]
+    public int initChance;
+
+    [Range(0, 8)]
+    public int birthLimit;
+
+    [Range(0, 8)]
+    public int deathLimit;
+
+    [Range(1, 10)]
+    public int numSimulations;
+    private int count = 0;
+
+    private int[,] terrainMap;
+    public Vector3Int tilemapSize;
+
+    public Tilemap topMap;
+    public Tilemap botMap;
+    public Tile topTile;
+    public Tile bottomTile;
+
+    int width;
+    int height;
+
+    private void doSim() {
+        clearMap(false);
+        width = tilemapSize.x;
+        height = tilemapSize.y;
+
+        if (terrainMap == null) {
+            terrainMap = new int[width, height];
+            initPos();
+        }
+
+        for (int i = 0; i < numSimulations; i++) {
+            terrainMap = genTilePos(terrainMap);
+        }
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (terrainMap[x, y] == 1) {
+                    topMap.SetTile(new Vector3Int(-x + width / 2, -y + height / 2, 0), topTile);
+                } else {
+                    botMap.SetTile(new Vector3Int(-x + width / 2, -y + height / 2, 0), bottomTile);
+                }
+            }
+        }
+    }
+
+    private int[,] genTilePos(int[,] oldMap) {
+        int[,] newMap = new int[width, height];
+        int neighbour;
+        BoundsInt myB = new BoundsInt(-1, -1, 0, 3, 3, 1);
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                neighbour = 0;
+                foreach (var b in myB.allPositionsWithin) {
+                    if (b.x == 0 && b.y == 0) continue;
+                    if (x + b.x >= 0 && x+b.x < width && y+b.y >= 0 && y+b.y < height) {
+                        neighbour += oldMap[x + b.x, y + b.y];
+                    } else {
+                        neighbour++;
+                    }
+                }
+
+                if (oldMap[x, y] == 1) {
+                    if (neighbour < deathLimit) {
+                        newMap[x, y] = 0;
+                    } else {
+                        newMap[x, y] = 1;
+                    }
+                }
+
+                if (oldMap[x, y] == 0) {
+                    if (neighbour > birthLimit) {
+                        newMap[x, y] = 1;
+                    } else {
+                        newMap[x, y] = 0;
+                    }
+                }
+            }
+        }
+
+        return newMap;
+    }
+
+    private void initPos() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                terrainMap[x, y] = Random.Range(1, 101) < initChance ? 1 : 0;
+            }
+        }
+    }
+
+    private void clearMap(bool complete) {
+        topMap.ClearAllTiles();
+        botMap.ClearAllTiles();
+
+        if (complete) {
+            terrainMap = null;
+        }
     }
 
     // Update is called once per frame
     void Update() {
-
+        if (Input.GetMouseButtonDown(0)) {
+            doSim();
+        }
     }
 }
--- project1/Assets/Scripts/PlayerController.cs
+++ project1/Assets/Scripts/PlayerController.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    [SerializeField] private Sprite upSprite;
-    [SerializeField] private Sprite downSprite;
-    [SerializeField] private Sprite leftSprite;
-    [SerializeField] private Sprite rightSprite;
+
     private SpriteRenderer sprite;
     private Animator animator;
 
     [SerializeField] private float runSpeed = 0.1f;
     private float horizontalMovement = 0.0f;
     private float verticalMovement = 0.0f;
-    private float prevHMovement = 0.0f;
-    private float prevVMovement = 0.0f;
 
     // Start is called before the first frame update
     void Start() {
@@ -30,15 +25,9 @@ public class PlayerController : MonoBehaviour {
             if (hInput > 0) {
                 // Move right
                 horizontalMovement = runSpeed;
-
-                // Change sprite to face right
-                sprite.sprite = rightSprite;
             } else if (hInput < 0) {
                 // Move left
                 horizontalMovement = -runSpeed;
-
-                // Change sprite to face left
-                sprite.sprite = leftSprite;
             }
             // Only move left/right or up/down at once
             verticalMovement = 0.0f;
@@ -52,15 +41,9 @@ public class PlayerController : MonoBehaviour {
             if (vInput > 0) {
                 // Move up
                 verticalMovement = runSpeed;
-
-                // Change sprite to face up
-                sprite.sprite = upSprite;
             } else if (vInput < 0) {
                 // Move down
                 verticalMovement = -runSpeed;
-
-                // Change sprite to face down
-                sprite.sprite = downSprite;
             }
             // Only move left/right or up/down at once
             horizontalMovement = 0.0f;
